3de4c10ef05cb5bdf2a7c64455c0593943ba177b
Haifan Deng <1123704529@qq.com>
2025-09-18 18:30:14 -0700

    modify raw_data_download.py

--- raw_data_download.py
+++ raw_data_download.py
@@ -47,22 +47,36 @@ def upsert_stock_price(connection, tickers, data):
     df = data.copy()
     if getattr(df.index, "tz", None) is not None:
         df.index = df.index.tz_convert("UTC").tz_localize(None)
-    sql = """
-        INSERT INTO stock_1d
-        (stock_id, `trade_date`, open, high, low, close, volume)
-        VALUES (%s,%s,%s,%s,%s,%s,%s)
-        ON DUPLICATE KEY UPDATE
-          open=VALUES(open),
-          high=VALUES(high),
-          low =VALUES(low),
-          close=VALUES(close),
-          volume=VALUES(volume)
-    """
+    create_sql = """
+                CREATE TABLE IF NOT EXISTS stock_1d (
+                    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+                    stock_id    INT NOT NULL,
+                    trade_date  TIMESTAMP NOT NULL,
+                    open        DECIMAL(10,4) NULL,
+                    high        DECIMAL(10,4) NULL,
+                    low         DECIMAL(10,4) NULL,
+                    close       DECIMAL(10,4) NOT NULL,
+                    volume      BIGINT UNSIGNED,
+                    UNIQUE KEY uniq_stock_date (stock_id, trade_date)
+                ) ENGINE=InnoDB;
+            """
+    insert_sql = """
+                INSERT INTO stock_1d
+                    (stock_id, `trade_date`, open, high, low, close, volume)
+                    VALUES (%s,%s,%s,%s,%s,%s,%s)
+                    ON DUPLICATE KEY UPDATE
+                        open=VALUES(open),
+                        high=VALUES(high),
+                        low =VALUES(low),
+                        close=VALUES(close),
+                        volume=VALUES(volume)
+            """
 
     batch, BATCH_SIZE = [], 1000
     cursor = connection.cursor()
 
-    try: 
+    try:
+        cursor.execute(create_sql)
         for ticker in tickers:
             col_level0 = df.columns.get_level_values(0)
             if ticker not in set(col_level0):
@@ -77,14 +91,16 @@ def upsert_stock_price(connection, tickers, data):
                 v = row.get("Volume", None)
                 batch.append((sid, ts, float(o), float(h), float(l), float(c), v))
                 if len(batch) >= BATCH_SIZE:
-                    cursor.executemany(sql, batch)
+                    cursor.executemany(insert_sql, batch)
                     batch.clear()
             
             if batch:
-                cursor.executemany(sql, batch)
+                cursor.executemany(insert_sql, batch)
                 batch.clear()
     except Exception as e:
         print(f"Error: {e}")
+        connection.rollback()
+        raise
     finally:
         cursor.close()
 
@@ -98,13 +114,16 @@ def fetch_price(tickers, period, interval):
     except Exception as e:
         print(f"Error: {e}")
         return None
+
     
 if __name__ == "__main__":
     connection = get_connection()
-    tickers = get_all_tickers(connection)
+    try:
+        tickers = get_all_tickers(connection)
 
-    period = "7y"
-    interval = "1d"
-    data = fetch_price(tickers, period, interval)
-    upsert_stock_price(connection, tickers, data)
-    connection.close()
+        period = "7y"
+        interval = "1d"
+        data = fetch_price(tickers, period, interval)
+        upsert_stock_price(connection, tickers, data)
+    finally:
+        connection.close()
