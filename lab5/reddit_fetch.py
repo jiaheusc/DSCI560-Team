4d86562c0a31bb7137cf27d81069f3ee439d0f41
rexzhang13 <minghuaz@usc.edu>
2025-09-25 14:09:02 -0700

    v2
    extract text from image

--- lab5/reddit_fetch.py
+++ lab5/reddit_fetch.py
@@ -8,6 +8,10 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from collections import Counter
 import sys
+import pytesseract
+from PIL import Image
+import requests
+from io import BytesIO
 
 #run once
 nltk.download('punkt')
@@ -49,6 +53,16 @@ def extract_keywords(text, top_n = 5):
     return ",".join([w for w, _ in freq.most_common(top_n)])
 
 
+def extract_text_from_image(url):
+    try:
+        response = requests.get(url, timeout=10)
+        img = Image.open(BytesIO(response.content))
+        text = pytesseract.image_to_string(img)
+        return clean_text(text)
+    except Exception as e:
+        print(f"[!] OCR failed for {url}: {e}")
+        return ""
+
 
 
 def fetch_reddit(total_post):
@@ -62,7 +76,9 @@ def fetch_reddit(total_post):
         remaining = total_post - fetched
         limit = min(max_limit, remaining)
 
-        posts = tech_sub.hot(limit=limit, params={"after": last_post})
+        posts = tech_sub.hot(limit = limit, params = {"after": last_post})
+
+        print(f'fetching and saving {limit} posts')
 
         for post in posts:
             post_id = post.id
@@ -70,17 +86,20 @@ def fetch_reddit(total_post):
             content = clean_text(post.selftext)
             author = mask_username(str(post.author))
             timestamp = datetime.fromtimestamp(post.created_utc)
+            image_text = ""
+            if post.url.endswith((".jpg", ".jpeg", ".png")):
+                image_text = extract_text_from_image(post.url)
 
-            title_content = f"{title} {content}"
-            keywords = extract_keywords(title_content )
+            combined_content = f"{title} {content} {image_text}"
+            keywords = extract_keywords(combined_content )
 
             
             sql_query = """
-            INSERT IGNORE INTO reddit_posts (id, title, content, author_masked, timestamp, keywords)
-            VALUES (%s, %s, %s, %s, %s, %s)
+            INSERT IGNORE INTO reddit_posts (id, title, content, author_masked, timestamp, keywords, image_text)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
             """
 
-            cursor.execute(sql_query, (post_id, title, content, author, timestamp, keywords))
+            cursor.execute(sql_query, (post_id, title, content, author, timestamp, keywords, image_text))
             db.commit()
 
             last_post = post.name
@@ -90,7 +109,7 @@ def fetch_reddit(total_post):
             print('extract all the posts')
             break  
         else:
-            print(f'right now fetched {fetched} posts')
+            print(f'right now already fetched {fetched} posts')
 
 
 if __name__ == "__main__":
