7d8f99738b1275f04ebe7d1292326dd58a3dc08c
Haifan Deng <1123704529@qq.com>
2025-11-14 15:57:57 -0800

    add add user_role

--- lab10/groupchat_app_src/sql/schema.sql
+++ lab10/groupchat_app_src/sql/schema.sql
@@ -3,16 +3,16 @@ CREATE DATABASE IF NOT EXISTS groupchat CHARACTER SET utf8mb4 COLLATE utf8mb4_un
 CREATE USER IF NOT EXISTS 'chatuser'@'localhost' IDENTIFIED BY 'chatpass';
 GRANT ALL PRIVILEGES ON groupchat.* TO 'chatuser'@'localhost';
 FLUSH PRIVILEGES;
-
 USE groupchat;
 
 CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50) NOT NULL UNIQUE,
-  prefer_name VARCHAR(50),
-  basic_info TEXT,
-  password_hash VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    user_role ENUM('user', 'therapist', 'operator') NOT NULL DEFAULT 'user',
+    prefer_name VARCHAR(50),
+    basic_info TEXT,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS chat_groups (
@@ -42,7 +42,8 @@ CREATE TABLE IF NOT EXISTS messages (
   content TEXT NOT NULL,
   is_bot BOOLEAN DEFAULT FALSE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
+  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL,
+  CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES chat_groups(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
