8391a7dfc7edfd4c19efc04996b7fefb90bb68bc
jiaheusc <jiahecai@usc.edu>
2025-12-01 17:48:09 -0800

    Update api.js

--- lab10/groupchat_app_src/frontend/groupchat-react-app/src/api.js
+++ lab10/groupchat_app_src/frontend/groupchat-react-app/src/api.js
@@ -70,12 +70,6 @@ export const getMessages = (groupId, token) =>
 
 export const sendMessageToGroup = (content, groupId, token) =>
   api("/messages", "POST", { content, group_id: groupId }, token);
-export async function getUserGroups(userId, token) {
-  const res = await fetch(`/api/therapist/users/${userId}/groups`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  return res.json();
-}
 
 export async function getUserGroups(userId, token) {
   const res = await fetch(`/api/therapist/users/${userId}/groups`, {
